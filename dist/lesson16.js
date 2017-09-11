4512c39a22d9424718562a933918192e5d651c11
cong le <cong.le@jeppesen.com>
2017-09-11 15:27:23 -0600

    lesson 16

---
+++ dist/lesson16.js
@@ -0,0 +1,7 @@
+"use strict";
+var colorNames = ['red', 'blue', 'green', 'yellow', 'purple'];
+for (var _i = 0, colorNames_1 = colorNames; _i < colorNames_1.length; _i++) {
+    var color = colorNames_1[_i];
+    console.log(color);
+}
+//# sourceMappingURL=lesson16.js.map
